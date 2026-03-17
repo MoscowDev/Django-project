7303b1d236b456cd1109317f56b304deef34eac9
MoscowDev <mosesogomegbunam@gmail.com>
2026-03-17 05:26:31 +0100

    new commit

--- wallet/admin.py
+++ wallet/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Wallet, Transaction, Ledger
+
 
 # Register your models here.
+
+@admin.register(Wallet)
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ['wallet_number','account_number','balance', 'currency', 'status']
+    list_editable = ['status', 'balance', 'currency']
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+        list_display = [ 'sender', 'receiver', 'amount',  'status']
+        list_editable = ['status', 'amount']
+
+@admin.register(Ledger)
+class LedgerAdmin(admin.ModelAdmin):
+        list_display = ['transaction', 'entry_type', 'amount']
+        list_editable = ['entry_type', 'amount']
