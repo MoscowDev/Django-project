4ff132c4a7e1e4a7c45bff1f992df82f73c57557
MoscowDev <mosesogomegbunam@gmail.com>
2026-03-17 05:27:08 +0100

    new commit

--- wallet/models.py
+++ wallet/models.py
@@ -32,6 +32,11 @@ class Wallet(models.Model):
     status = models.BooleanField(default = True)
     created = models.DateTimeField(auto_now_add = True)
 
+    def __str__(self):
+        return f" {self.wallet_number} "
+
+
+
 class Transaction(models.Model):
         TRANSACTION_CHOICES = (
         ('DEBIT', 'Debit'),
@@ -43,7 +48,7 @@ class Transaction(models.Model):
         ('FAILED', 'Failed'),
 
         )
-        reference_number = models.UUIDField(max_length=100, unique=True, editable=False, blank=False, null=True,default=generate_reference_number())
+        # reference_number = models.UUIDField(max_length=100, unique=True, editable=False, blank=False, null=True,default=generate_reference_number())
         reference = models.CharField(max_length = 100 )
         transaction_type = models.CharField(max_length = 6, choices= TRANSACTION_CHOICES, default='DEBIT')
         amount = models.DecimalField(max_digits=10,decimal_places=2)
@@ -54,6 +59,8 @@ class Transaction(models.Model):
         created_at = models.DateTimeField(auto_now_add=True)
         idempotency_key = models.UUIDField(unique=True, editable=False, blank=True, null=True)
 
+        def __str__(self):
+            return f" {self.reference} "
 
 class Ledger(models.Model):
     TRANSACTION_CHOICES  = (
@@ -66,3 +73,6 @@ class Ledger(models.Model):
     wallet = models.ForeignKey(Wallet, on_delete=models.PROTECT)
     entry_type = models.CharField(max_length=100, choices = TRANSACTION_CHOICES, default='DEBIT')
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f" {self.transaction} {self.entry_type} {self.amount} "
