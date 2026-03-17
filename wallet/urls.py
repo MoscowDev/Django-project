73d1dd102e4cdebe0474065c811d9ed901ab9ee3
MoscowDev <mosesogomegbunam@gmail.com>
2026-03-17 05:27:40 +0100

    new commit

--- wallet/urls.py
+++ wallet/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import transfer_wallet
-
+# from QuickPay.wallet.views import transfer_wallet
+from .views import dashboard
 
 urlpatterns = [
 
     path('transfer/', transfer_wallet, name='transfer'),
+
+    path("dashboard/", dashboard, name="dashboard"),
 ]
