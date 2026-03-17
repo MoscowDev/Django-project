e2c8c21694bd1e9b1dbdd2a89c61c05a24f822dc
MoscowDev <mosesogomegbunam@gmail.com>
2026-03-17 05:33:43 +0100

    new commit

--- urls.py
+++ urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'QuickPay Admin'
+admin.site.site_title = 'QuickPay Admin'
+admin.site.index_title = ' Welcome to QuickPay Admin'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
