c8940046ecf38e3f365df46e5a263627f755d5ad
MoscowDev <mosesogomegbunam@gmail.com>
2026-03-17 05:27:55 +0100

    new commit

--- wallet/views.py
+++ wallet/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render
 from rest_framework import status
 
 from .models import Wallet
-from .serializer import  WalletTransferSerializer
+from .serializer import WalletTransferSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-from wallet.services.intra_transfer_service import transfer_wallet_to_wallet
+from .services.intra_transfer_service import transfer_wallet_to_wallet
 from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
+from .serializer import DashboardSerializer
+
+
+from .services.dashboard_service import get_dashboard_data
+
 
 # Create your views here.
 
@@ -24,18 +29,29 @@ def transfer_wallet(request):
     amount = serializer.validated_data['amount']
     idempotency_key = serializer.validated_data['idempotency_key']
     description = serializer.validated_data['description']
+    receiver_wallet = serializer.validated_data['receiver_wallet']
     # description = serializer.Charfield(max_length=400, required=False)
 
-    receiver =get_object_or_404(Wallet, wallet_number = serializer.validated_data['wallet_number'])
+    receiver = get_object_or_404(Wallet, wallet_number = receiver_wallet.pk)
     tx = transfer_wallet_to_wallet(sender, receiver, amount, idempotency_key,description = description)
+
 
     return Response(
         {
         'amount': tx.amount,
         'status': tx.status,
+        'wallet_number': tx.wallet_number,
         'description': tx.description,
         'reference': tx.reference,
         'created_at': tx.created_at
     }, status =  status.HTTP_201_CREATED
 
     )
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def dashboard(request):
+    user = request.user
+    dashboard_data = get_dashboard_data(user)
+    serializer = DashboardSerializer(dashboard_data)
+    return Response(serializer.data, status = status.HTTP_200_OK)
