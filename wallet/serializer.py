603758ac9dd3b8fa5ce281959d88acb9e6d65588
MoscowDev <mosesogomegbunam@gmail.com>
2026-03-17 05:27:32 +0100

    new commit

--- wallet/serializer.py
+++ wallet/serializer.py
@@ -1,14 +1,12 @@
 from rest_framework import serializers
-
-from wallet.models import Wallet
+from .models import Wallet, Transaction
 
 
 class WalletTransferSerializer(serializers.Serializer):
-    wallet_number = serializers.CharField(max_length=10)
+    receiver_wallet = serializers.CharField(max_length=10)
     amount = serializers.DecimalField(max_digits=10, decimal_places=2)
     idempotency_key = serializers.UUIDField()
-    description = serializers.CharField(max_length=225,required=False)
-
+    description = serializers.CharField(max_length=225, required=False)
 
     def validate_amount(self, value):
         if value < 0:
@@ -22,3 +20,36 @@ class WalletTransferSerializer(serializers.Serializer):
             raise Exception("User wallet does not exist")
 
         return receiver_wallet
+
+
+class RecentTransactionSerializer(serializers.Serializer):
+    class Meta:
+        model = Transaction
+        fields = [
+            'receiver',
+            'amount',
+            'reference',
+            'status',
+            'created_at',
+            'transaction_type'
+        ]
+
+
+class DashboardSerializer(serializers.Serializer):
+    message = serializers.CharField(max_length=55)
+    wallet = serializers.CharField(max_length=10)
+    balance = serializers.DecimalField(max_digits=10, decimal_places=2)
+    currency = serializers.CharField(max_length=3)
+    status = serializers.CharField(max_length=100)
+    transactions = RecentTransactionSerializer(many=True)
+
+    # class Meta:
+    #     model = Wallet
+    #     fields = [
+    #         'message',
+    #         'wallet_number',
+    #         'balance',
+    #         'currency',
+    #         'status',
+    #         'transactions'
+    #     ]
